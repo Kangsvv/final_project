3a9e773a91ceb2bdadb3a04055987cc8dffb1181
Kangsvv <tjrdnjsdlekt@naver.com>
2022-06-27 16:11:33 +0900

    dao, dto, service added

--- src/main/java/kh/spring/controller/MemberController.java
+++ src/main/java/kh/spring/controller/MemberController.java
@@ -1,26 +1,13 @@
 package kh.spring.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 
 @Controller
-public class HomeController {
-
-	@RequestMapping("/")
-	public String home() {
-
-		return "home";
-	}
+@RequestMapping("/member/")
+public class MemberController {
+	
 	@ExceptionHandler //예외 공동 처리
 	public String exceptionHandler(Exception e) {//NumberFormatException.class, SQLException.class
 		e.printStackTrace();
